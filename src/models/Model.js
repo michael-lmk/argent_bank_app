3f31407031ead0f297501284190b1e4c2ca7920b
michael <michael@lmksystem.fr>
2023-01-30 23:20:54 +0100

    transfert pc

---
+++ src/models/Model.js
@@ -0,0 +1,30 @@
+
+
+import constants from "../constants/Conf.js";
+
+/**
+ * Class of model for get data
+ */
+class Model {
+
+  /**
+   * Start process for get data from backend 
+   * @param {string} path params of url 
+   * @returns 
+   */
+  async get(path) {
+
+    let result = await fetch(constants.base_url + path)
+      .then((response) => response.json())
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.log("erreur fetch to api : " + error);
+      });
+    return result
+  }
+
+}
+
+export default Model;
